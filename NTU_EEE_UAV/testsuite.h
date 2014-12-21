bd859d1321dc2c1625b4f6987b24b61b67292c79
ericweiye <eric_weiye@live.com>
2014-12-21 14:29:43 +0800

    test for mavconn modified protocol

--- NTU_EEE_UAV/testsuite.h
+++ NTU_EEE_UAV/testsuite.h
@@ -11,17 +11,17 @@ extern "C" {
 
 #ifndef MAVLINK_TEST_ALL
 #define MAVLINK_TEST_ALL
-static void mavlink_test_common(uint8_t, uint8_t, mavlink_message_t *last_msg);
+static void mavlink_test_pixhawk(uint8_t, uint8_t, mavlink_message_t *last_msg);
 static void mavlink_test_NTU_EEE_UAV(uint8_t, uint8_t, mavlink_message_t *last_msg);
 
 static void mavlink_test_all(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
-	mavlink_test_common(system_id, component_id, last_msg);
+	mavlink_test_pixhawk(system_id, component_id, last_msg);
 	mavlink_test_NTU_EEE_UAV(system_id, component_id, last_msg);
 }
 #endif
 
-#include "../common/testsuite.h"
+#include "../pixhawk/testsuite.h"
 
 
 static void mavlink_test_uwb_position_estimate(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
